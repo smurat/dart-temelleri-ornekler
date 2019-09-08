bd6d5d5c2d160ac293d2883dc4b1d9bc78a73101
smurat <smuratcelik@smurat.github.com>
2019-09-08 17:00:08 +0300

    aralığı 150'ye çıkar

--- 1-100_arası_15ile_bolunen_sayilarin_karesi.dart
+++ 1-100_arası_15ile_bolunen_sayilarin_karesi.dart
@@ -1,5 +1,5 @@
 main(List<String> args) {
-  for (int i = 1; i < 100; i++)
+  for (int i = 1; i < 150; i++)
     if (i % 3 == 0 && i % 5 == 0) {
       print("15'e bölünebilen $i'nin karesi = ${i * i}");
     }
